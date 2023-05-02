8b661dd5c88d04d4c4d6feceab015a38bd1909b7
Elvan Hidayat <elvanhd@gmail.com>
2023-05-03 03:21:13 +0700

    load user and set token

--- src/context/auth/AuthState.js
+++ src/context/auth/AuthState.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 import React, { useReducer } from 'react';
-import { CLEAR_ERRORS, REGISTER_FAIL, REGISTER_SUCCESS } from '../types';
+import setAuthToken from '../../utils/setAuthToken';
+import {
+  AUTH_ERROR,
+  CLEAR_ERRORS,
+  REGISTER_FAIL,
+  REGISTER_SUCCESS,
+  USER_LOADED,
+} from '../types';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
 
@@ -16,7 +23,22 @@ const AuthState = (props) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   // Load User
-  const loadUser = () => console.log('loaduser');
+  const loadUser = async () => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+
+    try {
+      const res = await axios.get('/api/auth');
+
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({ type: AUTH_ERROR });
+    }
+  };
 
   // Register User
   const register = async (formData) => {
@@ -33,6 +55,8 @@ const AuthState = (props) => {
         type: REGISTER_SUCCESS,
         payload: res.data,
       });
+
+      loadUser();
     } catch (err) {
       dispatch({
         type: REGISTER_FAIL,
--- src/context/auth/authReducer.js
+++ src/context/auth/authReducer.js
@@ -1,7 +1,20 @@
-import { CLEAR_ERRORS, REGISTER_FAIL, REGISTER_SUCCESS } from '../types';
+import {
+  AUTH_ERROR,
+  CLEAR_ERRORS,
+  REGISTER_FAIL,
+  REGISTER_SUCCESS,
+  USER_LOADED,
+} from '../types';
 
 export default (state, action) => {
   switch (action.type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload,
+      };
     case REGISTER_SUCCESS:
       localStorage.setItem('token', action.payload.token);
       return {
@@ -11,6 +24,7 @@ export default (state, action) => {
         loading: false,
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return {
         ...state,
