febe9840ea8b7e67bc633f2282f2debb25c3f7be
Elvan Hidayat <elvanhd@gmail.com>
2023-05-02 19:05:07 +0700

    auth context and initial state

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import About from './components/pages/About';
 import Home from './components/pages/Home';
+import AuthState from './context/auth/AuthState';
 import ContactState from './context/contact/ContactState';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
---
+++ src/context/auth/AuthState.js
@@ -0,0 +1,41 @@
+import React, { useReducer } from 'react';
+import AuthContext from './authContext';
+import authReducer from './authReducer';
+
+const AuthState = (props) => {
+  const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    user: null,
+    error: null,
+  };
+
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  // Load User
+
+  // Register User
+
+  // Login User
+
+  // Logout
+
+  // Clear Errors
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthState;
