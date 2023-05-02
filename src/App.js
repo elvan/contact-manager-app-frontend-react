ab8b074dc7658a51f07417ee9e1f61d108a498bd
Elvan Hidayat <elvanhd@gmail.com>
2023-05-03 02:29:04 +0700

    register page, login page and alert component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Fragment } from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import './App.css';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Alerts from './components/layout/Alerts';
 import Navbar from './components/layout/Navbar';
 import About from './components/pages/About';
 import Home from './components/pages/Home';
+
+import './App.css';
+import AlertState from './context/alert/AlertState';
 import AuthState from './context/auth/AuthState';
 import ContactState from './context/contact/ContactState';
 
@@ -11,17 +16,22 @@ const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className='container'>
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className='container'>
+                <Alerts />
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
